5664ead069b207f568c9de5dedca93bde6aeae4e
Allan Montilla <allangmr7@gmail.com>
2021-08-02 22:46:13 -0500

    Counter App Basic React v1.0
    Counter App Basic React v1.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import {React} from 'react'
 import reactDom from 'react-dom';
 import CounterApp from './CounterApp'
-//import PrimeraApp from './PrimeraApp'
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-//reactDom.render(<PrimeraApp saludos="Hola Soy Allan"/>, divRoot);
 
-reactDom.render(<CounterApp  value={20} />, divRoot);
+reactDom.render(<CounterApp  value={0} />, divRoot);
